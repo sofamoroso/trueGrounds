9bbc92981dd2fca68fbd05c987b2dd3fc3f4de75
sofia amoroso <sofi79a0@stud.kea.dk>
2025-04-08 11:41:34 +0200

    active style

--- script.js
+++ script.js
@@ -9,13 +9,18 @@ const normalBtn = document.getElementById("normalMode");
 ecoBtn.addEventListener("click", () => {
   html.classList.remove("normal");
   html.classList.add("eco");
+  ecoBtn.classList.add("active");
+  normalBtn.classList.remove("active");
 });
 
 // Apply Normal Theme
 normalBtn.addEventListener("click", () => {
   html.classList.remove("eco");
   html.classList.add("normal");
+  ecoBtn.classList.remove("active");
+  normalBtn.classList.add("active");
 });
+
 
 // populate sections
 
